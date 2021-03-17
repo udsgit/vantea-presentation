d040c9403e77517159f571f5dea1b6fb6c764ad7
Agustin T Rocha <agustin.torres@vantea.com>
2021-03-17 10:04:20 +0000

    Added TW and shopify. WIP Shopify

--- src/components/MainTheme.vue
+++ src/components/MainTheme.vue
@@ -1,13 +1,19 @@
 <template>
   <VueRouter/>
+  <Tailwind/>
+  <Shopify/>
 </template>
 
 <script>
 import VueRouter from "./VueRouter";
+import Tailwind from "@/components/Tailwind";
+import Shopify from "@/components/Shopify";
 
 export default {
   name: "MainTheme",
   components: {
+    Shopify,
+    Tailwind,
     VueRouter
   }
 }
